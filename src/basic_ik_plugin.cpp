268e51fc309874c902179ab61d17201d94881237
Valeriy Novytskyy <valeriy.novytskyy@outlook.com>
2024-03-03 10:05:55 -0800

    sh

--- src/basic_ik_plugin.cpp
+++ src/basic_ik_plugin.cpp
@@ -134,7 +134,7 @@ MatrixXd IKPlugin::solve(Matrix4d pose) const
     .matrix()
     .block(0, 3, 3, 1);
 
-  /*Vector3d shoulderPose =
+  Vector3d shoulderPose =
     (
       Translation3d(0, 0, 0) *
       AngleAxisd(base, Vector3d::UnitZ()) *
@@ -144,7 +144,7 @@ MatrixXd IKPlugin::solve(Matrix4d pose) const
     .matrix()
     .block(0, 3, 3, 1);
 
-  Vector3d elbowPose
+  /*Vector3d elbowPose
     (
       Translation3d(0, 0, 0) *
       AngleAxisd(base, Vector3d::UnitZ()) *
